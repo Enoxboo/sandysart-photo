44674a6f3dcc0477cc273207ff27ebfff29a7b0e
Enoxboo <matteo.marquant@ynov.com>
2025-11-04 22:32:51 +0100

    feat: create React frontend with Home page and photo display

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+    return (
+        <div className="container">
+            <h1>À propos</h1>
+            <p>Présentation de Sandy's Art Photographies</p>
+        </div>
+    );
+}
+
+export default About;
---
+++ frontend/src/pages/Admin.jsx
@@ -0,0 +1,10 @@
+function Admin() {
+    return (
+        <div className="container">
+            <h1>Administration</h1>
+            <p>Page admin en construction...</p>
+        </div>
+    );
+}
+
+export default Admin;
